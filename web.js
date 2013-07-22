05927edc92a34fae3f02e09dc65c21ca4a26b7c6
jjem <jjem2013@gmail.com>
2013-07-22 20:36:53 +0000

    updated web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var msg = fs.readFileSync('index.html').toString();
 response.send(msg);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
