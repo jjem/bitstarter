fbc412d6488d87f28f7e3a0dc098f246f48fe23c
jjem <jjem2013@gmail.com>
2013-08-10 10:41:43 +0000

    added staging confg

--- web.js
+++ web.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var fs = require('fs');
+var auth = require('http-auth');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-var msg = fs.readFileSync('index.html').toString(); 
-response.send(msg);
+var basic = auth({
+    authRealm : "Private area.",
+    // username is mia, password is supergirl.
+    authList : ['jjem:{SHA}UpDB29AH3oPEm+S/nQEB3i1N17Q=']
 });
 
+var protect = process.env.PROTECT || false;
 var port = process.env.PORT || 8080;
+
+app.get('/', function(request, response) {
+    var msg = fs.readFileSync('index.html').toString();
+    if(protect){
+	basic.apply(request,response, function (username){
+	    response.send(msg);
+	});
+    }
+    else{
+	response.send(msg);
+    }
+});
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
